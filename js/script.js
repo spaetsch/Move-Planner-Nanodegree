66857bf3ef49ff251622dcadd9253295c14d4441
spaetsch <spaetsch@gmail.com>
2015-03-10 17:58:52 -0700

    Add first attempt at NYT API integration

--- js/script.js
+++ js/script.js
@@ -22,8 +22,28 @@ function loadData() {
 
     var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     
-
     $body.append('<img class="bgimg" src="' + streetviewURL + '">' );
+
+    //NYT AJAX request
+    var nytimesURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newest&api-key=6ae7eb48c46d8608cb61f452c36a9bc2:7:71561887"
+
+    $.getJSON(nytimesURL, function(data){
+        console.log(data);
+
+        $nytElem.text("New York Times articles");
+        articles = data.response.docs;
+
+        for (var i=0; i < articles.length; i++){
+            var article = articles[i];
+            console.log(article.web_url);
+            $nytElem.append('<li class = "article">' +
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>');
+        };
+            
+
+        });
+
 
     return false;
 };
