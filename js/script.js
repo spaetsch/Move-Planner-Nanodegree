2d788dc8d1afad7cd6a8458b71388b896825969f
spaetsch <spaetsch@gmail.com>
2015-03-10 19:35:36 -0700

    Add wikipedia API request

--- js/script.js
+++ js/script.js
@@ -11,9 +11,7 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
-
-    // YOUR CODE GOES HERE!
+    // load Google streetview
     var streetStr = $("#street").val();
     var cityStr = $("#city").val();
     var address = streetStr + ", " + cityStr;
@@ -28,21 +26,45 @@ function loadData() {
     var nytimesURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newest&api-key=6ae7eb48c46d8608cb61f452c36a9bc2:7:71561887"
 
     $.getJSON(nytimesURL, function(data){
-        console.log(data);
-
-        $nytElem.text("New York Times articles");
+        $nytElem.text("New York Times articles about " +cityStr);
         articles = data.response.docs;
 
         for (var i=0; i < articles.length; i++){
             var article = articles[i];
-            console.log(article.web_url);
+            //console.log(article.web_url);
             $nytElem.append('<li class = "article">' +
                 '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
                 '<p>' + article.snippet + '</p>');
-        };
-            
+            };
+        //error handling
+        }).error(function(e){
+            $nytHeaderElem.text("New York Times articles could not be loaded");
+    });
 
-        });
+    var wikiURL = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + cityStr + 
+        "&format=json&callback=wikiCallback";
+
+    console.log("wikiURL: ", wikiURL);
+  
+    $.ajax({
+        url: wikiURL,
+        dataType: "jsonp",
+        //jsonp: "callback", //redundant here
+        success: function(response){
+            var articleList = response[1];
+            console.log("here 1");
+            for (var i =0; i < articleList.length; i++){
+                articleStr = articleList[i];
+                console.log("wiki ", articleStr);
+                var URL = "http://en.wikipedia.org/wiki/" + articleStr;
+                $wikiElem.append('<li><a href="' +URL+ '">' + articleStr + '</a></li>');
+            };
+        }
+
+
+    })
+
+
 
 
     return false;
